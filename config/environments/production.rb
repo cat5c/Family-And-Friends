f576d418b564150082db4232c7982be00a7dab34
Joe Cassiere <jcassiere91@gmail.com>
2017-08-10 23:29:14 -0400

    more configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,11 +84,11 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
+      s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
+      s3_region: ENV.fetch('AWS_REGION')
     }
   }
   if ENV["RAILS_LOG_TO_STDOUT"].present?
